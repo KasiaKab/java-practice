deca6a2582c872d33a68a997750d5f366469e95d
KasiaKab <kkate2402@gmail.com>
2025-09-09 04:47:33 +0200

    refactor(guessgame): move scanner and messages to ConsoleReader

--- src/main/java/com/kasiakab/guessgame/GameLogic.java
+++ src/main/java/com/kasiakab/guessgame/GameLogic.java
@@ -1,41 +1,35 @@
 package com.kasiakab.guessgame;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class GameLogic {
 
 
     private static final int MAX_ATTEMPTS = 5;
 
-    private static final Scanner scanner = new Scanner(System.in);
     private static Random rand = new Random();
 
-
     public static void play() {
-        System.out.println("Welcome to the guess game!"
-                + "\nPlease enter a number between 0 and 100");
+        ConsoleReader.welcomeMessage();
 
         int secretNumber = rand.nextInt(101);
         int counter = 0;
 
         while (counter < MAX_ATTEMPTS) {
-            System.out.println("Please enter a number between 0 and 100");
-            int inputNumber = scanner.nextInt();
+            int inputNumber = ConsoleReader.collectNumber();
             counter++;
 
             if (inputNumber < secretNumber) {
-                System.out.println("Attempt: " + counter + "Too low! Try a higher number.");
+                ConsoleReader.toLowMessage(counter);
             } else if (inputNumber > secretNumber) {
-                System.out.println("Attempt: " + counter + "Too high! Try a lower number.");
+                ConsoleReader.toHighMessage(counter);
             } else {
-                System.out.println("Congratulations! You guessed it!");
+                ConsoleReader.winMessage();
                 break;
             }
         }
         if (counter == MAX_ATTEMPTS)
-            System.out.println("You have reached the maximum number of attempts!" +
-                    "\nGame over! The secret number was: " + secretNumber);
+            ConsoleReader.gameOverMessage(secretNumber);
 
     }
 }
