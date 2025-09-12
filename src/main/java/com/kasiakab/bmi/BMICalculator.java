db00a6de49e450dd7e16a7e73973d20a170c6a1e
KasiaKab <kkate2402@gmail.com>
2025-09-12 06:44:24 +0200

    feat(bmi): translate BMI categories to English

--- src/main/java/com/kasiakab/bmi/BMICalculator.java
+++ src/main/java/com/kasiakab/bmi/BMICalculator.java
@@ -7,9 +7,9 @@ public class BMICalculator {
     }
 
     public String printCategory(double bmi) {
-        if (bmi < 18.5) return "Niedowaga";
-        else if (bmi < 25) return "Norma";
-        else if (bmi < 30) return "Nadwaga";
-        else return "Otyłość";
+        if (bmi < 18.5) return "Underweight";
+        else if (bmi < 25) return "Normal weight";
+        else if (bmi < 30) return "Overweight";
+        else return "Obesity";
     }
 }
