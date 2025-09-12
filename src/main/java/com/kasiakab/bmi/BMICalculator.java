ea58834feb72e4ca058a0e1ebc721f0386ff410d
KasiaKab <kkate2402@gmail.com>
2025-09-12 06:36:23 +0200

    feat(bmi): add BMI category classification in BMICalculator

--- src/main/java/com/kasiakab/bmi/BMICalculator.java
+++ src/main/java/com/kasiakab/bmi/BMICalculator.java
@@ -5,4 +5,11 @@ public class BMICalculator {
     public double calculateBMI(double weight, double height) {
         return weight / (height * height);
     }
+
+    public String printCategory(double bmi) {
+        if (bmi < 18.5) return "Niedowaga";
+        else if (bmi < 25) return "Norma";
+        else if (bmi < 30) return "Nadwaga";
+        else return "Otyłość";
+    }
 }
