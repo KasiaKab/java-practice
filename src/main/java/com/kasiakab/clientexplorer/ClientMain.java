925db44eceaeac3acefe81dca9796950e53b92c9
KasiaKab <kkate2402@gmail.com>
2025-09-17 20:12:05 +0200

    feat(clientexplorer): add Main class as application entry point

---
+++ src/main/java/com/kasiakab/clientexplorer/ClientMain.java
@@ -0,0 +1,9 @@
+package com.kasiakab.clientexplorer;
+
+public class ClientMain {
+    public static void main(String[] args) {
+
+        ClientApp app = new ClientApp();
+        app.run();
+    }
+}
