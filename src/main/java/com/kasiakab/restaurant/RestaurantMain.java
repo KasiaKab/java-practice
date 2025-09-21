13ad4e76afb4bee2d51eecde4737c47f67f04baa
KasiaKab <kkate2402@gmail.com>
2025-09-21 07:43:34 +0200

    feat(restaurant): add RestaurantMain as entry point

---
+++ src/main/java/com/kasiakab/restaurant/RestaurantMain.java
@@ -0,0 +1,9 @@
+package com.kasiakab.restaurant;
+
+public class RestaurantMain {
+    public static void main(String[] args) {
+
+        RestaurantApp app = new RestaurantApp();
+        app.run();
+    }
+}
