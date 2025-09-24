99707a4abd6671c733c0f0873af66b07882cbcb3
KasiaKab <kkate2402@gmail.com>
2025-09-24 07:18:41 +0200

    feat(quiz): add QuizApp, QuizMain, file loading with Stream API, console interaction and scoring

---
+++ src/main/java/com/kasiakab/quiz/QuizApp.java
@@ -0,0 +1,52 @@
+package com.kasiakab.quiz;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class QuizApp {
+
+    public void run() {
+
+        List<Question> questions = loadQuestionsFromFile("questions.txt");
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        QuizService quizService = new QuizService(questions);
+
+        int score = quizService.runQuiz(consoleHandler);
+        consoleHandler.showResult(score, questions.size());
+        consoleHandler.close();
+
+    }
+
+    private List<Question> loadQuestionsFromFile(String fileName) {
+
+        InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (is == null) {
+            throw new IllegalArgumentException("Plik nie znaleziony: " + fileName);
+        }
+
+        List<String> lines = new BufferedReader(new InputStreamReader(is))
+                .lines()
+                .collect(Collectors.toList());
+
+        return lines.stream()
+                //  | to w wyrażeniach regularnych (regex) specjalny znak, więc musimy go zdublować (\\|)
+                .map(line -> line.split("\\|"))
+                .filter(parts -> parts.length == 2)
+                .map(parts -> new Question(parts[0], parts[1]))
+                .collect(Collectors.toList());
+
+                /*
+                [linia1, linia2, linia3]
+                → stream() → [pyt|odp, pyt|odp, pyt|odp]
+                → split("\\|") → [[pyt, odp], [pyt, odp], [pyt, odp]]
+                → filter → [[pyt, odp], [pyt, odp]]
+                → map → [Question, Question]
+                → collect() → List<Question>
+                */
+    }
+
+}
---
+++ src/main/java/com/kasiakab/quiz/QuizMain.java
@@ -0,0 +1,9 @@
+package com.kasiakab.quiz;
+
+public class QuizMain {
+    public static void main(String[] args) {
+
+        QuizApp quizApp = new QuizApp();
+        quizApp.run();
+    }
+}
