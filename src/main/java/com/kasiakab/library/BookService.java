2c6f5d97cb07fd87c09b0d882666865b413b377d
KasiaKab <kkate2402@gmail.com>
2025-09-27 19:20:04 +0200

    feat(library): add BookService as business layer

---
+++ src/main/java/com/kasiakab/library/BookService.java
@@ -0,0 +1,26 @@
+package com.kasiakab.library;
+
+import java.util.Optional;
+
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(Book book) {
+        bookRepository.addBook(book);
+    }
+
+    public Optional<Book> findBookByIsbn(String isbn) {
+        return bookRepository.findBookByIsbn(isbn);
+    }
+
+    public void removeBookByIsbn(String isbn) {
+        bookRepository.removeBookByIsbn(isbn);
+    }
+
+
+}
