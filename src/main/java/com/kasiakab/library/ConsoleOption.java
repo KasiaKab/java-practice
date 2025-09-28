420d4037921bc82350792435874f68fe771d5143
KasiaKab <kkate2402@gmail.com>
2025-09-28 10:17:44 +0200

    feat(library): add ConsoleOption enum for menu

---
+++ src/main/java/com/kasiakab/library/ConsoleOption.java
@@ -0,0 +1,21 @@
+package com.kasiakab.library;
+
+public enum ConsoleOption {
+
+    ADD_BOOK("Add a new book"),
+    FIND_BOOK("Find a book by ISBN"),
+    REMOVE_BOOK("Remove a book by ISBN"),
+    SHOW_ALL_BOOKS("Show all books"),
+    EXIT("Exit the application");
+
+    private final String description;
+
+    ConsoleOption(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
