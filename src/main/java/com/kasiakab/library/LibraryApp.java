0464780e962fbb172b100d3e29af6260f87c13fe
KasiaKab <kkate2402@gmail.com>
2025-09-28 16:39:22 +0200

    feat(library): implement console application with menu

---
+++ src/main/java/com/kasiakab/library/LibraryApp.java
@@ -0,0 +1,89 @@
+package com.kasiakab.library;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LibraryApp {
+
+    private final BookService bookService = new BookService(new BookRepository());
+    private final ConsoleHandler consoleHandler = new ConsoleHandler();
+
+
+    public void run() {
+        while (true) {
+            consoleHandler.showMessage("== Welcome to Library App ==");
+            consoleHandler.showMessage("Please chose the option:");
+            consoleHandler.displayOption();
+            ConsoleOption selectedOption = consoleHandler.readOption();
+            switch (selectedOption) {
+                case ADD_BOOK -> handleAddBook();
+                case FIND_BOOK -> handleFindBook();
+                case REMOVE_BOOK -> handleRemoveBook();
+                case SHOW_ALL_BOOKS -> handleShowAllBooks();
+                case EXIT -> {
+                    consoleHandler.showMessage("Goodbye!");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void handleShowAllBooks() {
+        List<Book> allBooks = bookService.getAllBooks();
+        if (allBooks.isEmpty()) {
+            consoleHandler.showMessage("No books found!");
+        }  else {
+            consoleHandler.showMessage("All books in the library:");
+            allBooks.forEach(book -> consoleHandler.showMessage(book.toString()));
+        }
+
+    }
+
+    private void handleRemoveBook() {
+        consoleHandler.showMessage("Please enter the ISBN of a book you would like to remove:");
+        String isbn = consoleHandler.readString().trim();
+        try {
+            bookService.removeBookByIsbn(isbn);
+            consoleHandler.showMessage("Removed the book successfully.");
+        } catch (BookNotFoundException e) {
+            consoleHandler.showMessage("Error: " + e.getMessage());
+        }
+    }
+
+    private void handleFindBook() {
+        consoleHandler.showMessage("Please enter the ISBN:");
+        String isbn = consoleHandler.readString().trim();
+
+        Optional<Book> book = bookService.findBookByIsbn(isbn);
+        if (book.isPresent()) {
+            consoleHandler.showMessage("Found: " + book.get());
+        } else {
+            consoleHandler.showMessage("Book with ISBN " + isbn + " not found!");
+        }
+    }
+
+    private void handleAddBook() {
+        consoleHandler.showMessage("Please enter the book title:");
+        String title = consoleHandler.readString().trim();
+
+        consoleHandler.showMessage("Please enter the author:");
+        String author = consoleHandler.readString().trim();
+
+        consoleHandler.showMessage("Please enter the ISBN:");
+        String isbn = consoleHandler.readString().trim();
+
+        createBook(title, author, isbn);
+    }
+
+    private void createBook(String title, String author, String isbn) {
+        try {
+            Book book = new Book(title, author, isbn);
+            bookService.addBook(book);
+            consoleHandler.showMessage("Book added successfully!");
+        } catch (IllegalArgumentException e) {
+            consoleHandler.showMessage("Error: " + e.getMessage());
+        }
+    }
+
+}
+
---
+++ src/main/java/com/kasiakab/library/LibraryMain.java
@@ -0,0 +1,8 @@
+package com.kasiakab.library;
+
+public class LibraryMain {
+    public static void main(String[] args) {
+        LibraryApp app = new LibraryApp();
+        app.run();
+    }
+}
