69eafd203531758475087ed8afade29c5b275e9b
KasiaKab <kkate2402@gmail.com>
2025-09-29 18:44:03 +0200

    feat(library): add GENERATE_REPORT option to menu

--- src/main/java/com/kasiakab/library/ConsoleOption.java
+++ src/main/java/com/kasiakab/library/ConsoleOption.java
@@ -6,6 +6,7 @@ public enum ConsoleOption {
     FIND_BOOK("Find a book by ISBN"),
     REMOVE_BOOK("Remove a book by ISBN"),
     SHOW_ALL_BOOKS("Show all books"),
+    GENERATE_RAPORT("Generate and save library report to file"),
     EXIT("Exit the application");
 
     private final String description;
